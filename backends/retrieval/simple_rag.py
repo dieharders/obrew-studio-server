a7341ba333f85d956a90aaa9ce70c8827f1e45c9
DIEHARDERS <cybrobo@gmail.com>
2025-04-16 19:40:47 -0700

    HBAI-330 Use saved embedding_model when creating/updating new embeddings

--- backends/retrieval/simple_rag.py
+++ backends/retrieval/simple_rag.py
@@ -6,22 +6,23 @@ from inference.helpers import apply_query_template
 class SimpleRAG:
     def __init__(
         self,
-        collections: List[Collection],
+        collection: Collection,
         embed_fn: Callable[[str], List[float]],
         llm_fn: Callable[[str], str],
     ):
         self.embed_fn = embed_fn
         self.llm_fn = llm_fn
-        self.collections = collections
+        self.collection = collection
 
     def query(
         self, question: str, system_message: str, template: str, top_k: int = 5
     ) -> str:
         query_embedding = self.embed_fn(question)
-        # @TODO loop thru and search each collection (query_embeddings)
-        collection = self.collections[0]
-        results = collection.query(query_embeddings=[query_embedding], n_results=top_k)
+        results = self.collection.query(
+            query_embeddings=[query_embedding], n_results=top_k
+        )
         documents = results.get("documents", [[]])[0]
+        # @TODO Override the template based "strategy"? Otherwise user has to know which templates go with which strategy.
         prompt = apply_query_template(
             template=template, query=question, context_list=documents
         )
