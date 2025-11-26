089b87553c311288783e2dc4c721122ae7909ab0
dieharders <cybrobo@gmail.com>
2025-11-26 00:03:02 -0800

    HBAI-419: Add comment, remove log.

--- backends/ui/public/init.js
+++ backends/ui/public/init.js
@@ -6,7 +6,6 @@ async function mountInitialPage() {
 
 // Mount initial page
 document.addEventListener('DOMContentLoaded', event => {
-  console.log('@@ DOMContentLoaded')
   // Listen for pywebview api to be ready
   window.addEventListener('pywebviewready', async () => {
     // Mount initial page
