97c452744a346f7538f1a78720c84c0083e2f5f2
Sorob Raissi <cybrobo@gmail.com>
2026-02-09 17:23:27 -0800

    HBAI-463: Support email search  (#140)
    * init email search implementation
    
    * HBAI-463: Up ver
    
    * - total_matches is misleading
    - Missing body in default search_fields
    - Type hint on main doesn't match convention
    - Early break skews items_searched count
    
    * update email search strat logic
    
    * Clarified search_fields description.
    Added field alias normalization.
    
    * Fix items_searched.
    Removed use_regex.
    
    * Fix misleading truncated field.
    Add conversationId to searchable fields.
    
    * Up ver
    
    * add comment

--- backends/tools/built_in_functions/__init__.py
+++ backends/tools/built_in_functions/__init__.py
@@ -28,6 +28,7 @@ from . import (
     agent_ask,
     agent_chooser,
     widget_email,
+    email_grep,
     widget_calendar,
 )
 
@@ -66,5 +67,7 @@ TOOLS = {
     "agent_chooser.py": agent_chooser,
     # Widget tools
     "widget_email.py": widget_email,
+    # Email search tools
+    "email_grep.py": email_grep,
     "widget_calendar.py": widget_calendar,
 }
